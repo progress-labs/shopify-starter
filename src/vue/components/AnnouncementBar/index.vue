b67f6dc3ac2671256f6d37452e62f750ace99960
patrickbjohnson <pbj@progresslabs.co>
2022-03-14 17:48:33 -0400

    setup announcement component

---
+++ src/vue/components/AnnouncementBar/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="bg-black py-3 px-10 text-white"  v-if="isOpen">  
+    <div class="container text-sm text-center py-2 flex justify-between">
+      <div v-if="content" v-html="content"></div>
+      <button aria-label="Close" @click="close">X</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AnnouncementBar',
+  props: {
+    visible: {
+      type: Boolean,
+      required: false, 
+      default: true,
+    },
+    indexPageOnly: {
+      type: Boolean,
+      required: false, 
+      default: false,
+    },
+    content: {
+      type: String,
+      required: true
+    },
+  },
+  data() {
+    return {
+      isOpen: this.visible,  
+    }
+  },
+  methods: {
+    close() {
+      this.isOpen = false;
+    }
+  },
+  mounted() {
+    console.log(this.visible)
+  }
+}
+</script>
