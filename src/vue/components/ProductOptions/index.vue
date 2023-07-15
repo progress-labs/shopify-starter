7e703b8c3e5a204214cc0445fa48fe01de18e736
patrickbjohnson <pbj@progresslabs.co>
2023-07-15 11:56:56 -0400

    wip

---
+++ src/vue/components/ProductOptions/index.vue
@@ -0,0 +1,53 @@
+<script>
+import {mapState, mapActions} from "vuex";
+import productOptions from "../../mixins/productOptions";
+export default {
+  name: "ProductOptions",
+
+  props: {
+    foo: 'foo'
+  },
+  computed: {
+    ...mapState("product", ["selectedVariant"]),
+  },
+
+  watch: {
+    // variantToPurchase(newVal) {
+    //   this.setVariant(newVal);
+    // },
+  },
+
+  methods: {
+    ...mapActions("product", ["setProduct", "setVariant"]),
+  },
+
+  mounted() {
+    /**
+     * The setup in here relies on options to be selected,
+     * as the variants are filtered based on those selections. 
+     * In this case, we preselect a value assuming there is one variant available.
+     */
+    
+    // if (this.eligibleVariants.length === 1) {
+    //   const option = this.productData.options[0];
+      
+    //   this.selectedOptions = [{
+    //     type: option.name.toLowerCase(),
+    //     position: option.position, 
+    //     value: option.value[0]
+    //   }]
+    // }
+  },
+
+  render() {
+    return this.$slots.default({
+      // selectedVariant: this.selectedVariant,
+      // findVariantsByOptions: this.findVariantsByOptions,
+      // currentVariant: this.currentVariant,
+      // options: this.selectedOptions,
+      // isActiveOption: this.isActiveOption,
+      // isVisibleOption: this.isVisibleOption,
+    });
+  },
+};
+</script>
---
+++ src/vue/components/Test/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Test info!</div>
+</template>
+
+<script>
+import {mapState, mapActions} from "vuex";
+export default {
+  name: "TestThing",
+
+  props: {
+    foo: 'foo'
+  },
+  
+  computed: {
+    ...mapState("product", ["selectedVariant"]),
+  },
+
+  methods: {
+    ...mapActions("product", ["setProduct", "setVariant"]),
+  },
+
+  mounted() {
+    console.log('mounted -- test')
+  },
+};
+</script>
