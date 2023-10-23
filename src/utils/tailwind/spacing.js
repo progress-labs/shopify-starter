84b3a9c64b93dd7fde38c434c230370a9b607225
Ryan Seit <ryan@ryanseit.com>
2023-10-23 14:40:25 -0400

    Update spacing.js

--- src/utils/tailwind/spacing.js
+++ src/utils/tailwind/spacing.js
@@ -1,6 +1,4 @@
-const spacing = [...Array(100).keys()].reduce(
+export const spacing = [...Array(100).keys()].reduce(
   (a, v, idx) => ({...a, [idx * 5]: `${idx * 5}px`}),
   {},
 );
-
-export default spacing;
