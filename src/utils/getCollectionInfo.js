2e7b173268530cc9b8be013d747a593ed96af5eb
patrickbjohnson <pbj@progresslabs.co>
2024-02-22 19:42:36 -0500

    remove unused files

--- src/utils/getCollectionInfo.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default async handle => {
-  const url = `/collections/${handle}/products.json`;
-  return await axios.get(url).then(({data}) => data.products);
-};
--- src/utils/getProductInfo.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default async handle => {
-  const url = `/products/${handle}.js`;
-  return await axios.get(url).then(({data}) => data);
-};
