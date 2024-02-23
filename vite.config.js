f7b67a2cfce60c75dd812dffdeb9d74c702ac3de
patrickbjohnson <pbj@progresslabs.co>
2024-02-22 21:02:19 -0500

    build assets

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
         assetFileNames: "[name].[hash].min[extname]",
       },
     },
-    outDir: resolve(__dirname, "shopify/assets"),
+    outDir: resolve(__dirname, "assets"),
     emptyOutDir: false,
   },
 });
