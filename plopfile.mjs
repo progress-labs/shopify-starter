aa39f54398a5b0d975c2ab15f8b88c5f9f031414
patrickbjohnson <pbj@progresslabs.co>
2024-02-22 19:58:48 -0500

    update plop

--- plopfile.mjs
+++ plopfile.mjs
@@ -1,143 +1,144 @@
+const plopDir = "_plop";
 export default function (plop) {
-	// create your generators here
-  plop.setGenerator('Vue Component', {
-    description: 'Create a Vue Component',
+  // create your generators here
+  plop.setGenerator("Vue Component", {
+    description: "Create a Vue Component",
     prompts: [
       {
-        type: 'input',
-        name: 'name',
-        message: 'Component Name?'
+        type: "input",
+        name: "name",
+        message: "Component Name?",
       },
       {
-        type: 'confirm',
-        name: 'renderless',
-        message: 'Create renderless component?',
-        default: false
+        type: "confirm",
+        name: "renderless",
+        message: "Create renderless component?",
+        default: false,
       },
     ],
-    actions: (data) => {
-      let actions = []
+    actions: data => {
+      let actions = [];
       if (data.renderless) {
         actions.push({
-          type: 'add',
+          type: "add",
           path: `src/vue/components/{{ pascalCase name }}/index.vue`,
-          templateFile: 'plop/renderless-component.hbs'
-        })
+          templateFile: `${plopDir}/renderless-component.hbs`,
+        });
       } else {
         actions.push({
-          type: 'add',
-          path: 'src/vue/components/{{ pascalCase name }}/index.vue',
-          templateFile: 'plop/render-component.hbs'
-        })
+          type: "add",
+          path: "src/vue/components/{{ pascalCase name }}/index.vue",
+          templateFile: `${plopDir}/render-component.hbs`,
+        });
       }
-      return actions
-    }
-});
-  
-	plop.setGenerator('JavaScript - Utility', {
-		description: 'Generate a new utility file',
-		prompts: [
+      return actions;
+    },
+  });
+
+  plop.setGenerator("JavaScript - Utility", {
+    description: "Generate a new utility file",
+    prompts: [
       {
-        type: 'input',
-        name: 'name',
-        message: 'File name please'
+        type: "input",
+        name: "name",
+        message: "File name please",
       },
     ], // array of inquirer prompts
-		actions: [
+    actions: [
       {
-        type: 'add',
-        path: 'src/utils/{{ name }}.js',
-        templateFile: 'plop/util.hbs'
-      }
-    ]  // array of actions
-	});
-  
-	plop.setGenerator('Shopify - Section', {
-		description: 'Generate a new Shopify section',
-		prompts: [
+        type: "add",
+        path: "src/utils/{{ name }}.js",
+        templateFile: `${plopDir}/util.hbs`,
+      },
+    ], // array of actions
+  });
+
+  plop.setGenerator("Shopify - Section", {
+    description: "Generate a new Shopify section",
+    prompts: [
       {
-        type: 'input',
-        name: 'name',
-        message: 'File name please'
+        type: "input",
+        name: "name",
+        message: "File name please",
       },
     ], // array of inquirer prompts
-		actions: [
+    actions: [
       {
-        type: 'add',
-        path: 'shopify/sections/{{ kebabCase name }}.liquid',
-        templateFile: 'plop/section.hbs'
+        type: "add",
+        path: "shopify/sections/{{ kebabCase name }}.liquid",
+        templateFile: `${plopDir}/section.hbs`,
       },
       {
-        type: 'add',
-        path: 'src/css/sections/{{ kebabCase name }}.scss',
-        templateFile: 'plop/css.hbs'
-      }
-    ]  // array of actions
-	});
-  
-	plop.setGenerator('Shopify - Snippet', {
-		description: 'Generate a new Shopify snippet',
-		prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'File name please'
+        type: "add",
+        path: "src/css/sections/{{ kebabCase name }}.scss",
+        templateFile: `${plopDir}/css.hbs`,
       },
-    ], // array of inquirer prompts
-		actions: [
+    ], // array of actions
+  });
+
+  plop.setGenerator("Shopify - Snippet", {
+    description: "Generate a new Shopify snippet",
+    prompts: [
       {
-        type: 'add',
-        path: 'shopify/snippets/{{ kebabCase name }}.liquid',
-        templateFile: 'plop/snippet.hbs'
-      }
-    ]  // array of actions
-	});
-  
-	plop.setGenerator('CSS - Component', {
-		description: 'Generate a new CSS component',
-		prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'File name please'
+        type: "input",
+        name: "name",
+        message: "File name please",
       },
     ], // array of inquirer prompts
-		actions: [
+    actions: [
       {
-        type: 'add',
-        path: 'src/css/components/{{ kebabCase name }}.css',
-        templateFile: 'plop/css.hbs'
+        type: "add",
+        path: "shopify/snippets/{{ kebabCase name }}.liquid",
+        templateFile: `${plopDir}/snippet.hbs`,
       },
+    ], // array of actions
+  });
+
+  plop.setGenerator("CSS - Component", {
+    description: "Generate a new CSS component",
+    prompts: [
       {
-        type: 'append',
-        path: 'src/css/main.scss',
-        pattern: '/* Components */',// keep this line - the template will append bellow your comment
-        template: "@import \"components/{{ kebabCase name }}\";", // replace your append template here
-      }
-    ]  // array of actions
-	});
-  
-  plop.setGenerator('CSS - Utility', {
-		description: 'Generate a new CSS utility',
-		prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'File name please'
+        type: "input",
+        name: "name",
+        message: "File name please",
       },
     ], // array of inquirer prompts
-		actions: [
+    actions: [
       {
-        type: 'add',
-        path: 'src/css/utils/{{ kebabCase name }}.css',
-        templateFile: 'plop/css.hbs'
+        type: "add",
+        path: "src/css/components/{{ kebabCase name }}.scss",
+        templateFile: `${plopDir}/css.hbs`,
       },
       {
-        type: 'append',
-        path: 'src/css/main.scss',
-        pattern: '/* Utiliies */',// keep this line - the template will append bellow your comment
-        template: "@import \"utils/{{ kebabCase name }}\";", // replace your append template here
-      }
-    ]  // array of actions
-	});
-};
+        type: "append",
+        path: "src/entrypoints/main.scss",
+        pattern: "/* Components */", // keep this line - the template will append bellow your comment
+        template: `@import "components/{{ kebabCase name }}";`, // replace your append template here
+      },
+    ], // array of actions
+  });
+
+  plop.setGenerator("CSS - Utility", {
+    description: "Generate a new CSS utility",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "File name please",
+      },
+    ], // array of inquirer prompts
+    actions: [
+      {
+        type: "add",
+        path: "src/css/utils/{{ kebabCase name }}.scss",
+        templateFile: `${plopDir}/css.hbs`,
+      },
+      {
+        type: "append",
+        path: "src/css/main.scss",
+        pattern: "/* Utiliies */", // keep this line - the template will append bellow your comment
+        template: '@import "utils/{{ kebabCase name }}";', // replace your append template here
+      },
+    ], // array of actions
+  });
+}
