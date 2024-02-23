c9c3a8e0d40462f5a98c3e2eed122a9cd11c67ec
patrickbjohnson <pbj@progresslabs.co>
2024-02-22 19:44:45 -0500

    remove logs

--- src/vue/store/cart.js
+++ src/vue/store/cart.js
@@ -101,13 +101,10 @@ const actions = {
   },
 
   addItem: ({commit, state, dispatch}, payload) => {
-    console.log("payload: ", payload);
     if (state.loading) return;
     commit("cartLoading");
 
     let products = Array.isArray(payload) ? payload : [payload];
-
-    console.log("payload: ", products);
 
     axios
       .post(
